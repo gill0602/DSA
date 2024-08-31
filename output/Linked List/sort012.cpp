eaa9e8472eb4787a7a7b63050ae1f2aa11a11d82
Sidharth Gill <sidharthgill19june@gmail.com>
2024-09-01 00:43:23 +0530

    New Code

--- output/Linked List/sort012.cpp
+++ output/Linked List/sort012.cpp
@@ -54,3 +54,34 @@ Node* sortLL(Node* head)
 
 
 }
+
+Node * ptr0, * ptr1 ,* ptr2,*p0,*p1,*p2;
+    ptr0 = new Node(0);
+    ptr1 = new Node(0);
+    ptr2 = new Node(0);
+    p0=ptr0;
+    p1=ptr1;
+    p2=ptr2;
+
+    Node * tmp=head;
+    while(tmp){
+        if(tmp->data==0){
+            ptr0->next=tmp;
+            ptr0=ptr0->next;
+        }
+        else if(tmp->data==1){
+            ptr1->next=tmp;
+            ptr1=ptr1->next;
+        }
+        else{
+            ptr2->next=tmp;
+            ptr2=ptr2->next;
+        }
+        tmp=tmp->next;
+    } 
+    ptr0->next=p1->next;
+    ptr1->next=p2->next;
+    ptr2->next=NULL;
+    return p0->next;
+        }
+};
