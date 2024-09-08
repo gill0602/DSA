f84637edc9021d01581bcfd032deca6b6e9b2d4d
Sidharth Gill <sidharthgill19june@gmail.com>
2024-09-08 23:43:59 +0530

    New Updates

--- output/Linked List/138DeepCopy.cpp
+++ output/Linked List/138DeepCopy.cpp
@@ -1,3 +1,6 @@
+
+// First Approach O(n) space and O(n) time 
+
 Node* copyRandomList(Node* head) {
         if(head==NULL){
             return NULL;
@@ -16,3 +19,36 @@ Node* copyRandomList(Node* head) {
         }
         return old_to_new[head];
     }
+
+// Second Approach Interweaving O(1) space and O(n) time
+
+if(!head){
+            return NULL;
+        }
+        Node* tmp=head;
+        while(tmp){
+            Node* newNode=new Node(tmp->val);
+            newNode->next=tmp->next;
+            tmp->next=newNode;
+            tmp=tmp->next->next;
+        }
+        tmp=head;
+        while(tmp){
+            if(tmp->random){
+                tmp->next->random=tmp->random->next;
+            }
+            tmp=tmp->next->next;
+        }
+        Node* old_head=head;
+        Node* new_head=head->next;
+        Node* curr_old=old_head;
+        Node* curr_new=new_head;
+
+        while(curr_old){
+            curr_old->next=curr_old->next->next;
+            curr_new->next=curr_new->next?curr_new->next->next:NULL;
+            curr_old=curr_old->next;
+            curr_new=curr_new->next;
+        }
+        return new_head;
+        
