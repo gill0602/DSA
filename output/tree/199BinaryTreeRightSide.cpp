743d31594af75db1bb394ac107c32a538e1725f9
Sidharth Gill <162141143+gill0602@users.noreply.github.com>
2026-03-18 13:46:55 +0530

    Update 199BinaryTreeRightSide.cpp

--- output/tree/199BinaryTreeRightSide.cpp
+++ output/tree/199BinaryTreeRightSide.cpp
@@ -41,3 +41,62 @@ public:
         return right;
     }
 };
+
+#Optimal Approach
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// TreeNode definition
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int v) : val(v), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    // Recursive function to get left view
+    void leftDFS(TreeNode* node, int level, vector<int>& res) {
+        // Base case: null node
+        if (!node) return;
+
+        // If this is the first node at the current level
+        if (res.size() == level)
+            res.push_back(node->val);
+
+        // Explore left subtree first
+        leftDFS(node->left, level + 1, res);
+
+        // Then explore right subtree
+        leftDFS(node->right, level + 1, res);
+    }
+
+    // Recursive function to get right view
+    void rightDFS(TreeNode* node, int level, vector<int>& res) {
+        if (!node) return;
+
+        if (res.size() == level)
+            res.push_back(node->val);
+
+        // Explore right subtree first
+        rightDFS(node->right, level + 1, res);
+
+        // Then explore left subtree
+        rightDFS(node->left, level + 1, res);
+    }
+
+    vector<int> leftView(TreeNode* root) {
+        vector<int> res;
+        leftDFS(root, 0, res);
+        return res;
+    }
+
+    vector<int> rightView(TreeNode* root) {
+        vector<int> res;
+        rightDFS(root, 0, res);
+        return res;
+    }
+};
+
